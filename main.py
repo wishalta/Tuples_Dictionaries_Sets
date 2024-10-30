ad1513f2690783a8da2e87b2fea68dfcd33082aa
wishalta <rolandas.lebed@gmail.com>
2024-10-30 09:52:49 +0200

    3 dictionary tasks done

--- main.py
+++ main.py
@@ -52,27 +52,88 @@
 # 2
 
 import datetime
+#
+# filmas = {
+#     'Pavadinimas': 'The Substance',
+#     'Rezisierius': 'Coralie Fargeat',
+#     'Biudzetas': ['$', 17.5, 'million'],
+#     'Uzdarbys': ['$', 39.1, 'million'],
+#     'Zanras': 'body horror',
+#     'Ilgis': '141 minutes',
+#     'Isleidimo metai': [20, 'September', 2024],
+#     'Aktoriu sarasas': ['Demi Mūr', 'Margaret Qualley', 'Denis Kveidas', 'Hugo Diego Garcia']
+# }
+#
+# print(filmas)
+# pelnas = filmas['Uzdarbys'][1] - filmas['Biudzetas'][1]
+# print(pelnas)
+#
+# count = 0
+#
+# for x in filmas['Aktoriu sarasas']:
+#     count += 1
+#
+# print(f'Aktoriu kiekis: {count}')
+#
+# print(f'Metu filmui: {filmas['Isleidimo metai'][2] - datetime.date.today().year}')
 
-filmas = {
-    'Pavadinimas': 'The Substance',
-    'Rezisierius': 'Coralie Fargeat',
-    'Biudzetas': ['$', 17.5, 'million'],
-    'Uzdarbys': ['$', 39.1, 'million'],
-    'Zanras': 'body horror',
-    'Ilgis': '141 minutes',
-    'Isleidimo metai': [20, 'September', 2024],
-    'Aktoriu sarasas': ['Demi Mūr', 'Margaret Qualley', 'Denis Kveidas', 'Hugo Diego Garcia']
+# 3
+
+knyga1 = {
+    "pavadinimas": "Vėjų medžiotojai",
+    "autorius": "Jonas Pavardė",
+    "žanras": "Nuotykinė literatūra",
+    "kaina": 15.99,
+    "puslapių kiekis": 320,
+    "skyriai": ["Pradžia", "Kelionės pradžia", "Audros", "Nuotykiai jūroje", "Atrastos paslaptys", "Pabaiga"],
+    "išleidimo metai": 2021,
+    "galima rasti knygynuose": True
 }
 
-print(filmas)
-pelnas = filmas['Uzdarbys'][1] - filmas['Biudzetas'][1]
-print(pelnas)
+knyga2 = {
+    "pavadinimas": "Laiko šešėliai",
+    "autorius": "Viktorija Raštaitė",
+    "žanras": "Fantastika",
+    "kaina": 19.49,
+    "puslapių kiekis": 420,
+    "skyriai": ["Laiko įvadas", "Magiškos būtybės", "Tamsos užtemimas", "Drąsos išbandymas", "Galutinė kova", "Atgimimas"],
+    "išleidimo metai": 2018,
+    "galima rasti knygynuose": False
+}
 
-count = 0
+print(knyga1)
+print(knyga2)
 
-for x in filmas['Aktoriu sarasas']:
-    count += 1
+if knyga2['puslapių kiekis'] > knyga1['puslapių kiekis']:
+    print('Pirma knyga plonesne')
+elif knyga2['puslapių kiekis'] < knyga1['puslapių kiekis']:
+    print('Antra knyga plonesne')
+else:
+    print('Pusplaiu skaicius vienodas')
 
-print(f'Aktoriu kiekis: {count}')
+count1 = 0
+count2 = 0
 
-print(f'Metu filmui: {filmas['Isleidimo metai'][2] - datetime.date.today().year}')
+for x in knyga1['skyriai']:
+    count1 += 1
+
+for u in knyga2['skyriai']:
+    count2 += 1
+
+if count1 > count2:
+    print('Pirma knyga turi daugiau skyriu')
+if count1 < count2:
+    print('Antra knyga turi daugiau skyriu')
+else:
+    print('Lygus skyriu skaicius')
+
+if knyga1['kaina'] > knyga2['kaina']:
+    print('Knyga pirma yra brangesne')
+    dviguba = knyga2['kaina'] * 2
+    if dviguba > knyga1['kaina']:
+        print('Padvigubinta suma antros knygos, butu brangesne uz pirmos knygos kaina')
+if knyga1['kaina'] < knyga2['kaina']:
+    print('Knyga antra yra brangesne')
+    dviguba = knyga1['kaina'] * 2
+    if dviguba > knyga2['kaina']:
+        print('Padvigubinta suma pirmos knygos, butu brangesne uz antros knygos kaina')
